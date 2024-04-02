4a686990ccf5c4215eb813bc052c2ba8ee61a05a
AndersonCango <andersoncango09@gmail.com>
2024-04-02 07:15:12 -0500

    feat: add component title

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <span>Anderson Cango</span>
+      <p>I am software engineer student</p>
+    </div>
+  )
+}
+
+export default App
